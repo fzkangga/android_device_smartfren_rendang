666bd4d86135d1d5faaeed2a67a9c399f57722a5
fzkangga <titahpriyangga@gmail.com>
2019-06-13 14:21:22 +0700

    rendang: use own kernel

--- board/kernel.mk
+++ board/kernel.mk
@@ -2,6 +2,6 @@
 BOARD_DTBTOOL_ARGS := -2
 BOARD_KERNEL_IMAGE_NAME := zImage
 BOARD_KERNEL_SEPARATED_DT := true
-TARGET_KERNEL_SOURCE := kernel/wingtech/msm8916
-TARGET_KERNEL_CONFIG := lineageos_rendang_defconfig
+TARGET_KERNEL_SOURCE := kernel/lineage/msm8916
+TARGET_KERNEL_CONFIG := lineageos_rendang-n_defconfig
 BOARD_KERNEL_CMDLINE += sched_enable_hmp=1 phy-msm-usb.floated_charger_enable=1
