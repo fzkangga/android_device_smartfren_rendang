db8b9cc34fa7da8aeed017024bfb74d077cd8084
fzkangga <titahpriyangga@gmail.com>
2019-06-20 18:16:59 +0700

    rendang: update

--- product/perf.mk
+++ product/perf.mk
@@ -1,3 +1,7 @@
-# use 3 threads max for dex2oat
 PRODUCT_PROPERTY_OVERRIDES += \
-    ro.sys.fw.dex2oat_thread_count=3
+    ro.sys.fw.dex2oat_thread_count=2 \
+    ro.sys.fw.use_trim_settings=true \
+    ro.sys.fw.empty_app_percent=50 \
+    ro.sys.fw.trim_empty_percent=100 \
+    ro.sys.fw.trim_cache_percent=100 \
+    ro.sys.fw.trim_enable_memory=1073741824
--- product/qcom-camera.mk
+++ product/qcom-camera.mk
@@ -18,4 +18,6 @@ PRODUCT_COPY_FILES += \
 PRODUCT_PROPERTY_OVERRIDES += \
     persist.camera.cpp.duplication=false \
     persist.camera.hal.debug.mask=0 \
+    media.stagefright.legacyencoder=true \
+    media.stagefright.less-secure=true \
     ro.camera.sensors=ov5670_q5v41b,ov5648_q5v22e_rendang,gc2355_rendang
